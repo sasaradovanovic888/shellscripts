79b84d8fc7ce4c84d13b4cf503d3cea956b92833
sasaradovanovic888 <sasaradovanovic888@gmail.com>
2017-10-30 19:23:20 +0100

    Update

--- check_mysql_with_telnet.sh
+++ check_mysql_with_telnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
-echo "127.0.0.1 (localhost.localdomain)"
-echo "3306 MySQL port"
+echo "127.0.0.1 localhost.localdomain localhost"
+echo "MySQL default port 3306"
 telnet 127.0.0.1 3306
