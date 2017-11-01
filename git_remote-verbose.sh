4b960401056c7dccc9190afdc6b94c695e5e41bd
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-01 23:41:16 +0100

    added comment section about git remote command

--- git_remote-verbose.sh
+++ git_remote-verbose.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "the command git remote should respond with a fetch and push line"
+echo "git remote -v"
+echo "or"
+echo "git remote --verbose"
+echo
 #git remote --verbose
 git remote -v
