f3f509687f75dc778abee57ebc6e8c9c046bdd9a
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-05 21:56:48 +0100

    removed unused line for file extension java

--- editJavaFile.sh
+++ editJavaFile.sh
@@ -5,7 +5,6 @@
 # $file.java file is edited, compiled and script for starting is created
 file=$1
 rm --force $file.class # remove $file.class, ignore nonexistent files, never prompt
-java='java'
 editor='emacs' # editor='nano'
 touch $file.java
 # $editor $file.java &
