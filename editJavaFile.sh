a4b1e165880e22df69e6212013fa5ebcca44e34b
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-05 21:18:29 +0100

    emacs editor for editing java file

--- editJavaFile.sh
+++ editJavaFile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # $file.java file is edited, compiled and script for starting is created
 file=$1
+rm --force $file.class # remove $file.class, ignore nonexistent files, never prompt
 java='java'
 editor='emacs' # editor='nano'
 touch $file.java
