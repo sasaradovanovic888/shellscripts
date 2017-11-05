3490b5c74bd13abddf7df29930e8d5fd21a5402f
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-05 21:21:24 +0100

    added javac part

--- editJavaFile.sh
+++ editJavaFile.sh
@@ -6,3 +6,13 @@ java='java'
 editor='emacs' # editor='nano'
 touch $file.java
 $editor $file.java &
+# compile the file
+javac $file.java
+# create start shell script and insert initial code
+touch start_$file.sh
+cat <<EOF >start_$file.sh
+#!/bin/bash
+java $file
+EOF
+# chmod starting shell script
+chmod 755 start_$file.sh
