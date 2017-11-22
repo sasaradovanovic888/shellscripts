fd6eb01c80fc5e59a341a3b8a5f66b8f27c55c83
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-22 19:07:13 +0100

    Only two parameters for implementing  i

--- git/change_git_global_user_name_and_email.sh
+++ git/change_git_global_user_name_and_email.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Setting your Git GLOBAL user name and email for EVERY repository on your computer
-source git_settings_template.txt
-git config --global user.name "$firstname $lastname"
-git config --global user.email "$emailuser@$emailhost"
+source my_git_settings_template.txt
+git config --global user.name "$username"
+git config --global user.email "$email"
 . ./show_git_global_user_name_and_email.sh
