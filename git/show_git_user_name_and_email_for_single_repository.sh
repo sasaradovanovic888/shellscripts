1cba7416bf8628dae43b0ab7f005258f46ad61dc
Sale PC <sale@development.dev>
2017-11-22 17:15:15 +0100

    New look for reporting the git status for SINGLE repository

--- git/show_git_user_name_and_email_for_single_repository.sh
+++ git/show_git_user_name_and_email_for_single_repository.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Show Git user name and email for single repository
-git config user.name
-git config user.email
+clear
+echo "Show Git user name and email for SINGLE repository"
+single_repository_user_name=`git config user.name`
+single_repository_user_email=`git config user.email`
+message="Git config for SINGLE repository"
+echo "$message user.name  = $single_repository_user_name"
+echo "$message user.email = $single_repository_user_email"
