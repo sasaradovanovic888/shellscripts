e4997ed473b8e2cfc489e7c8edfe98bf8fd74b45
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2018-03-09 23:52:20 +0100

    Added function kotlinr()

--- kotlin/compile_kotlin_file.sh
+++ kotlin/compile_kotlin_file.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-echo Compiling, please wait...
-kotlinc $1 -include-runtime -d out.jar
-java -jar out.jar
+function kotlinr() {
+    echo Compiling, please wait...
+    kotlinc $1 -include-runtime -d out.jar
+    java -jar out.jar
+}
