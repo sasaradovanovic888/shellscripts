f9b15fe6eab863cdf0449983fd54d55fd2306b4b
Sale <sale@debiantest1.test.dev>
2018-03-09 22:41:38 +0100

    Added comments

--- manually_install_teamviewer_on_debian93.sh
+++ manually_install_teamviewer_on_debian93.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo 'Installation via command line'
+echo 'If you prefer to use the command line, or if there is no graphical installer available, you can use one of these commands as an administrator:'
+echo 'Just tupe: sudo [commands]'
 dpkg -i teamviewer_13.0.9865_amd64.deb
+echo 'In case dpkg indicates missing dependencies, complete the installation by executing the following command:'
 apt-get install -f
-
