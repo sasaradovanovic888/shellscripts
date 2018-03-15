0b4e085923331ee3f3b375227f040e94a0e84de1
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2018-03-15 01:46:01 +0100

    change into original repository with command cd $ORIGINAL_REPOSITORY

--- git/Configuring_a_remote_project_for_a_fork_and_sync.sh
+++ git/Configuring_a_remote_project_for_a_fork_and_sync.sh
@@ -2,6 +2,7 @@
 echo configure
 ORIGINAL_OWNER=''
 ORIGINAL_REPOSITORY=''
+cd $ORIGINAL_REPOSITORY
 git remote -v
 git remote add upstream https://github.com/$ORIGINAL_OWNER/$ORIGINAL_REPOSITORY.git
 git remote -v
