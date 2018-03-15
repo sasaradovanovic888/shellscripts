cbeef567cf10ec7b15a8a782247d205fc294684d
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2018-03-15 01:42:29 +0100

    Added git remote add upstream https://github.com/$ORIGINAL_OWNER/$ORIGINAL_REPOSITORY.git and sync

--- git/Configuring_a_remote_project_for_a_fork_and_sync.sh
+++ git/Configuring_a_remote_project_for_a_fork_and_sync.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+echo configure
+ORIGINAL_OWNER=''
+ORIGINAL_REPOSITORY=''
+git remote -v
+git remote add upstream https://github.com/$ORIGINAL_OWNER/$ORIGINAL_REPOSITORY.git
+git remote -v
+echo sync
+git fetch upstream
+git checkout master
+git merge upstream/master
