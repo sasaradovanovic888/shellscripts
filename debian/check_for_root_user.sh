cfc4fac6c5ac030ebdcd45c95976c3ddc4f052a3
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2018-03-30 13:11:31 +0200

    Initial commit

---
+++ debian/check_for_root_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+touch /checkroot 2>/dev/null
+
+uid=`stat -c "%u" /checkroot 2>/dev/null`
+
+if [ "$uid" = 0 ]
+then
+    echo "Root user"
+else
+    echo "Not a root user"
+fi
+rm /checkroot 2>/dev/null
